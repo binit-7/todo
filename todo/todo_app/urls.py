128d470d5e8ca7dba0ab0c594d8b22c0ee8e8ada
Binit Thapa <binit.28478@student.trinity.edu.np>
2026-01-02 20:58:22 +0545

    todo final app

--- todo/todo_app/urls.py
+++ todo/todo_app/urls.py
@@ -3,6 +3,8 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('add_task/', add_task, name='add_task'),
-    # path('mark_complete/<int:task_id>/', mark_complete, name='mark_complete'),
+    path('mark_complete/<int:id>/', mark_complete, name='mark_complete'),
     path('delete_task/<int:id>/', delete_task, name='delete_task'),
+    path('mark_undone/<int:id>/', mark_undone, name='mark_undone'),
+    path('edit_task/<int:id>/', edit_task, name='edit_task'),   
     ]
--- todo/todo_app/views.py
+++ todo/todo_app/views.py
@@ -1,10 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404
 from .models import Task
 # Create your views here.
 def home(request):  
-    task = Task.objects.filter(is_completed=False).order_by('-updated_at') # filter is used to fetch multiple data with different conditions
+    task = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    completed_task = Task.objects.filter(is_completed=True).order_by('-updated_at') # filter is used to fetch multiple data with different conditions
     context = {
-        'task' : task
+        'task' : task,
+        'completed_task' : completed_task
     }
     return render(request, 'home.html', context)
 
@@ -15,7 +19,30 @@ def add_task(request):
         return redirect('home')
 
 def delete_task(request, id):
-    task = Task.objects.get(id=id)
+    task = get_object_or_404(Task,id=id)
     task.delete()
-    return redirect('home')    
+    return redirect('home')
 
+def mark_complete(request, id):
+    task = Task.objects.get(id=id)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_undone(request,id):
+    task = get_object_or_404(Task,id=id)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, id):
+    get_task = get_object_or_404(Task, id=id)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+
+    
+    else:
+        return render(request, 'edit_task.html', {'task': get_task})
